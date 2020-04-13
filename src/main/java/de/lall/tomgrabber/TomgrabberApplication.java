b88b61324d952d870b3f7fe92bc2264435ca0bee
francoisbourgeois <mywerbung@gmx.de>
2020-04-13 13:58:48 +0200

    initial commit

---
+++ src/main/java/de/lall/tomgrabber/TomgrabberApplication.java
@@ -0,0 +1,13 @@
+package de.lall.tomgrabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomgrabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomgrabberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/lall/tomgrabber/TomgrabberApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lall.tomgrabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomgrabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
